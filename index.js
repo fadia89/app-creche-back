e8549fd43f1fb2dccda20e6962b44c9572aca53a
Fadia <guelbifedia@gmail.com>
2025-05-09 12:33:54 +0200

    Mise à jour de l'entrée principale du serveur (index.js)

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-import express  from 'express'
-import {connectDB , sequelize} from './dataBase/db.js';
+import express from 'express'
+import { connectDB, sequelize } from './dataBase/db.js';
 import usersRouter from './routes/usersRouter.js';
 import 'dotenv/config';
 import authRouter from './routes/authRouter.js';
 import parentsRouter from './routes/parentsRouter.js';
 import childrensRouter from './routes/childrensRouter.js';
 import eventsRouter from './routes/eventsRouter.js';
-import adminRouter from './routes/adminRouter.js';
+import activitiesRouter from './routes/activitiesRouter.js';
+import registationsRouter from './routes/registrationsRouter.js';
+import associations from './models/associations.js';
+import cors from 'cors';
+import path from 'path';
+
 
 
 
@@ -14,16 +19,25 @@ import adminRouter from './routes/adminRouter.js';
 // Middleware pour gérer les requêtes en JSON
 const app = express();
 const PORT = process.env.PORT || 8000;
+associations();
 
 app.use(express.json());  //  cette ligne pour que req.body fonctionne avec des données JSON
-app.use('/api', usersRouter ,parentsRouter, childrensRouter, eventsRouter,authRouter,adminRouter)
-//app.use ('/auth',authRouter)
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use('/images', express.static('public/images')) // afichage dans url tous ce qui se trouve dans le dossier public
 
 
+app.use('/api', usersRouter, parentsRouter, childrensRouter, eventsRouter, authRouter, activitiesRouter, registationsRouter)
 
+app.get('/public/images/:filename', (req, res) => {
+  const file = `public/images/${req.params.filename}`;
+  res.sendFile(path.resolve(file));
+});
 
 connectDB()
 
+
+
 app.listen(PORT, () => {
-    console.log(`Bonjour, bienvenue sur mon serveur Express! ${PORT}`);
-  });
+  console.log(`Bonjour, bienvenue sur mon serveur Express! ${PORT}`);
+});
