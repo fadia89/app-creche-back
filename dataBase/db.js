d9d9a25074118d51153be63f7bf6c55b97200a71
Fadia <guelbifedia@gmail.com>
2025-07-05 20:17:53 +0200

    Add .env.example and ignore .env for security

--- dataBase/db.js
+++ dataBase/db.js
@@ -1,28 +1,24 @@
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
-// Créer une instance de Sequelize
-const sequelize = new Sequelize({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  port: process.env.DB_PORT,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
-// Fonction de connexion à la base de données
 const connectDB = async () => {
   try {
-    await sequelize.authenticate();  // Essayer de s'authentifier avec la DB
+    await sequelize.authenticate();
     console.log("✅ Successfully connected to sequelizeDB");
   } catch (error) {
     console.error("❌ sequelizeDB connection error:", error);
-    process.exit(1); // Quitter si la connexion échoue
+    process.exit(1);
   }
 };
 
-// Exporter sequelize et connectDB pour utilisation ailleurs
-export  {sequelize, connectDB} ;
-
-
+export { sequelize, connectDB };
