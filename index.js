929c4bb0b8a953c6fa795ca9c1cbdac9647c3e5a
Fadia <guelbifedia@gmail.com>
2025-07-07 01:39:25 +0200

    changement de dossier publicpar tm

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
-import express from 'express';
-import path from 'path';
+import express from 'express'
+import { connectDB, sequelize } from './dataBase/db.js';
+import usersRouter from './routes/usersRouter.js';
+import 'dotenv/config';
+import authRouter from './routes/authRouter.js';
+import parentsRouter from './routes/parentsRouter.js';
+import childrensRouter from './routes/childrensRouter.js';
+import eventsRouter from './routes/eventsRouter.js';
+import adminRouter from './routes/adminRouter.js';
+import contactsRouter from './routes/contactsRouter.js';
+import documentsRouter from './routes/documentsRouter.js';
+import { User, Parent, Children, Event, Document } from './models/associations.js'
 import cors from 'cors';
+import registrationsRouter from './routes/registrationsRouter.js';
+import activitiesRouter from './routes/activitiesRouter.js';
+import path from 'path';
+import pagesRouter from './routes/pagesRouter.js';
+
+
+
+
+
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+// Middleware to handle JSON requests
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
-// Sert les images uploadées en statique
-app.use('/images', express.static(path.join(process.cwd(), 'public/images')));
+app.use(cors())
 
-// Sert les documents uploadés dans /tmp
-app.use('/documents', express.static('/tmp'));
 
-// Exemple route téléchargement directe (optionnel)
+
+app.use('/images', express.static('public/images'));
+
+app.use('/public', express.static('public'));
+
+app.use('/uploads', express.static('public/images'));
+
+app.use('/api', usersRouter, parentsRouter, childrensRouter, eventsRouter, authRouter, adminRouter, activitiesRouter, registrationsRouter, contactsRouter, documentsRouter, adminRouter)
+
+app.use('/api/pages', pagesRouter)
+
+
 app.get('/download/:type/:filename', (req, res) => {
   const { type, filename } = req.params;
-  let folder;
 
+  let folder = '';
   if (type === 'images') {
-    folder = path.join(process.cwd(), 'public/images');
+    folder = 'public/images';
   } else if (type === 'documents') {
-    folder = '/tmp'; // dossier où sont stockés les documents
+    folder = '/tmp';
   } else {
     return res.status(400).send('Type de fichier non pris en charge');
   }
 
-  const filePath = path.join(folder, filename);
+  const filePath = path.resolve(folder, filename);
 
   res.download(filePath, filename, (err) => {
     if (err) {
@@ -35,4 +63,13 @@ app.get('/download/:type/:filename', (req, res) => {
       res.status(404).send('Fichier non trouvé');
     }
   });
+});
+
+
+connectDB()
+
+
+
+app.listen(PORT, () => {
+  console.log(`Bonjour, bienvenue sur mon serveur Express! ${PORT}`);
 });
